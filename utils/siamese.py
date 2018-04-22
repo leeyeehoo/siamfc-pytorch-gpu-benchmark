a7984c3edb846a530903c9a7c085a5d5df7b8432
Yuhong Li <leeyh@minsky2-1.csl.illinois.edu>
2018-04-22 01:37:16 -0500

    tutorial of siamfc

---
+++ utils/siamese.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SiameseNet(nn.Module):
+
+    def __init__(self):
+        super(SiameseNet, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 96, 11, 2),
+            nn.BatchNorm2d(96),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(96, 256, 5, 1, groups=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(256, 384, 3, 1),
+            nn.BatchNorm2d(384),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(384, 384, 3, 1, groups=2),
+            nn.BatchNorm2d(384),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(384, 32, 3, 1, groups=2)
+        )
+        self.branch = nn.Sequential(
+            self.conv1,
+            self.conv2,
+            self.conv3,
+            self.conv4,
+            self.conv5
+        )
+        self.bn_adjust = nn.BatchNorm2d(1)
+    def forward_once(self, template):
+        template = self.branch(template)
+        return template
+    def xcorr(self,z, x):
+        out = []
+        for i in range(x.size(0)):
+            out.append(F.conv2d(x[i,:,:,:].unsqueeze(0), z[i,:,:,:].unsqueeze(0)))
+        return torch.cat(out, dim=0)
+    
+    def forward(self, z, x):
+        output1 = self.forward_once(z)
+        output2 = self.forward_once(x)
+        output = self.xcorr(z, x)
+        output = self.bn_adjust(output)
+        return output
+    
